eb3def1f35ccdd572f6bdee20147900b8e23c05c
Siddhant Jagtap <siddhant999123@gmail.com>
2025-02-24 10:50:07 +0530

    Deploy

--- db.js
+++ db.js
@@ -2,6 +2,7 @@ const mysql = require("mysql");
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.MYSQL_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
