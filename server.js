8c45d37d9c043f39f9068769cc172d950e3e6fab
Siddhant Jagtap <siddhant999123@gmail.com>
2025-02-24 15:17:48 +0530

    Deploy

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use("/api/contacts", contactRoutes);
 app.use("/api/company", companyRoutes);
 app.use('/api/otherinfo', otherInfoRoutes);
 
-app.get('/', function (req, res) {s
+app.get('/', function (req, res) {
     res.send('Hey, I am Node.JS');
   });
   app.listen(process.env.PORT, function () {
